728298bd790d6faac3b4a35befed85c753b761de
Sark <sarkkun@naver.com>
2023-01-02 16:26:21 +0900

    from chapter 1 to 3

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import morgan from "morgan";
+
+const PORT = 4000;
+
+const app = express();
+const logger = morgan("dev");
+
+const home = (req, res) => {
+  console.log("I will respond.");
+  return res.send("hello");
+};
+
+const login = (req, res) => {
+  return res.send("login");
+};
+
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
+
+const handleListening = () => console.log("Server listening on port 4000");
+
+app.listen(PORT, handleListening);
